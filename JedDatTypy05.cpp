7310c1906e63a7caf676894bb1417bc34342e5c8
bodnarb.04 <bodnarb.04@gmail.com>
2022-10-20 14:54:39 +0200

    Přidejte soubory projektu.

---
+++ JedDatTypy05.cpp
@@ -0,0 +1,52 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char x;
+    int male = 0, velke = 0, cisla = 0, operatory = 0, jiny = 0;
+    bool obsahuje = false;
+    cout << "Zadavej znaky zakoncena (\\): " << endl;
+    cin >> x;
+    while (x != '\\')
+    {
+        if (x >= 'A' && x <= 'Z')
+        {
+            ++velke;
+        }
+        else if (x >= 'a' && x <= 'z')
+        {
+            ++male;
+        }
+        else if (x >= '0' && x <= '9')
+        {
+            ++cisla;
+        }
+        else if (x == '+' || x == '-' || x == '*' || x == '/' || x == '%')
+        {
+            ++operatory;
+        }
+        else 
+        {
+            ++jiny;
+        }
+
+        if (x >= '0' && x <= '9' && x % 2 == 0)
+        {
+            obsahuje = true;
+        }
+
+        cin >> x;
+    }
+    if (obsahuje)
+    {
+        cout << "Ano obsahovalo sudou cifru " << "\nPocet malych " << male << "\nPocet velkych " << velke << "\nPocet cifer " << cisla << "\nPocet operatoru " << operatory << "\nPocet jinych " << jiny << endl;
+    }
+    else
+    {
+        cout << "Ne neobsahovalo sudou cifru " << "\nPocet malych " << male << "\nPocet velkych " << velke << "\nPocet cifer " << cisla << "\nPocet operatoru " << operatory << "\nPocet jinych " << jiny << endl;
+    }
+}
+
